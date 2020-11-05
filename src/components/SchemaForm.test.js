efd84d6c7bfc87e8ac4cf5caaac8b8eee487080e
Steve Cassidy <steve.cassidy@mq.edu.au>
2020-11-05 11:54:10 +1100

    Even more tests of form interactions.

--- src/components/SchemaForm.test.js
+++ src/components/SchemaForm.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import SchemaForm from './SchemaForm';
 import '@testing-library/jest-dom'
-import Enzyme, { shallow, mount, render } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 
 const formSpec = {
    schema: {
     type: 'object',
+    required: ['foo'],
     properties: { 
       foo: {
         title: "Foo",
@@ -46,3 +47,37 @@ it('renders a simple form', () => {
 
   wrapper.unmount()
 });
+
+
+it("shows a dialog if I click Next without filling out the form", () => {
+
+  const div = document.createElement('div');
+  const event = new Event("click")
+  const callback = jest.fn()
+  window.alert = jest.fn()
+  const wrapper = mount(<SchemaForm formSpec={formSpec} callbackFn={callback} />, div);
+  
+  wrapper.find('button').invoke('onClick')(event)
+  expect(callback).toHaveBeenCalledTimes(0)
+  expect(window.alert).toHaveBeenCalledTimes(1)
+  wrapper.unmount()
+})
+
+
+
+it("calls the callback fn if I click Next after filling out the form", () => {
+
+  const div = document.createElement('div');
+  const event = new Event("click")
+  const callback = jest.fn()
+  window.alert = jest.fn()
+  const wrapper = mount(<SchemaForm formSpec={formSpec} callbackFn={callback} />, div);
+  
+  const input = wrapper.find('input[id="#/properties/foo-input"]')
+  input.simulate('change', {target: {value: 'hello'}})
+  wrapper.find('button').invoke('onClick')(event)
+  expect(callback).toHaveBeenCalledTimes(1)
+  expect(callback).toHaveBeenCalledWith({foo: "hello"})
+  expect(window.alert).toHaveBeenCalledTimes(0)
+  wrapper.unmount()
+})
