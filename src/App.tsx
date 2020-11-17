42343d990b3cafe29b5428dc2e5bd70eec9bea2b
Steve Cassidy <steve.cassidy@mq.edu.au>
2020-11-17 17:47:10 +1100

    Add basic navigation via reach/router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Router, RouteComponentProps, Link } from "@reach/router"; 
 import FormSequence from './components/FormSequence'
+import ListObservations from './components/ListObservations'
 import './App.css';
+import logo from './faims-logo.png'
 
 import pocForms from './schema/pocForms.json'
-
 
 interface CompoundFormData {
   generalData: object,
@@ -14,16 +16,31 @@ const App = () => {
 
   const formComplete = (formData: any) => {
     console.log("FORM COMPLETE", formData)
-
   }
 
+  const Logo = (props: RouteComponentProps) => (<div id="logo"><img src={logo}/></div>)
+  const Forms = (props: RouteComponentProps) => (<FormSequence formSpec={pocForms} callbackFn={formComplete} />)
+  const ListPage = (props: RouteComponentProps)  => (<ListObservations />)
+
   return (
-    <Fragment>
-      <div>   
-          <FormSequence formSpec={pocForms} callbackFn={formComplete} />
-      </div>
-    </Fragment>
-  );
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/forms">Enter Observation</Link></li>
+          <li><Link to="/list">List Observations</Link></li>
+        </ul>
+      </nav>
+
+      <Router>
+        <Logo path="/" />
+        <Forms path="/forms" />
+        <ListPage path="/list" />
+      </Router>
+      
+    </div>
+     ) 
+    
 };
 
 export default App;
---
+++ src/components/ListObservations.tsx
@@ -0,0 +1,15 @@
+import React, {useState, Fragment} from 'react';
+
+
+const ListObservations = () => {
+
+    return (
+        <div>
+            A list of observations
+        </div>
+    )
+
+}
+
+
+export default ListObservations
